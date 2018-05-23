ea319e0b27d37caed32743dde307bd2d5dadf185
HerryLo <13995578046@163.com>
2018-05-23 14:23:32 +0800

    保存aside侧边栏

---
+++ src/components/common/asideMenu.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <el-row style="height: 100%;">
+        <el-col :span="4"  style="min-height: 100%; background-color: #324057;width:100%">
+            <div style="height:200px;text-align:center;">
+            <img 
+                src="https://avatars3.githubusercontent.com/u/20215975?s=400&u=70cd2ff9db05211275e5675f80f63d406ebb0ac2&v=4" 
+                alt=""
+                style="width:100px;margin-top:30px;border-radius: 15%;">
+            </div>
+            <el-menu 
+            :default-active="defaultActive"
+            background-color="rgb(50, 65, 87)"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            style="min-height: 100%;" 
+            theme="dark" 
+            router>
+            <el-submenu index="1">
+                <template slot="title"><i class="el-icon-message"></i>管理员</template>
+                <el-menu-item index="userAdmin">用户管理</el-menu-item>
+                <el-menu-item index="writeArt">文章编辑</el-menu-item>
+            </el-submenu>
+            </el-menu>
+        </el-col>
+		</el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'asideMenu',
+    computed: {
+        defaultActive: function(){
+        return this.$route.path.replace('/', '');
+        }
+    },
+}
+</script>
+
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,71 +1,28 @@
 <template>
-<el-container style="height: 100%;">
-  <el-aside width="200px" style="background-color: rgb(50, 65, 87);">
-    <div style="height:200px;text-align:center;">
-      <img 
-        src="https://avatars3.githubusercontent.com/u/20215975?s=400&u=70cd2ff9db05211275e5675f80f63d406ebb0ac2&v=4" 
-        alt=""
-        style="width:100px;margin-top:30px;border-radius: 15%;">
-    </div>
-    <el-menu 
-      :default-openeds="['1']"
-      default-active="2"
-      background-color="rgb(50, 65, 87)"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message">管理员</i></template>
-        <el-menu-item-group>   
-          <el-menu-item index="1-1" @click="toRouter('/userAdmin')">
-            用户管理
-          </el-menu-item>
-          <el-menu-item index="1-2" @click="toRouter('/writeArt')">
-            文章编辑
-          </el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-    </el-menu>
-  </el-aside>
-  
-  <el-container style="height: aoto;">
-    <el-header>
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>admin</span>
-    </el-header>
+  <el-container style="height: 100%;">
+    <el-aside width="200px" style="background-color: rgb(50, 65, 87);">
+      <aside-menu></aside-menu>
+    </el-aside>
     
-    <el-main style="background:#eee;">
-      <router-view />
-    </el-main>
+    <el-container style="height: aoto;">
+      <el-header>
+        <span>admin</span>
+      </el-header>
+      <el-main style="background:#eee;">
+        <router-view />
+      </el-main>
+    </el-container>
   </el-container>
-</el-container>
-
 </template>
 
 <script>
 import { artListAPI } from "../config/API";
+import asideMenu from './common/asideMenu'
 
 export default {
   name: "Index",
   data() {
-    return {
-      nav: [
-        {
-          index: "1",
-          name: "管理员",
-          routes: [
-            { index: "1-1", name: "用户管理", url: "/userAdmin" },
-            { index: "1-2", name: "文章编辑", url: "/writeArt" }
-          ]
-        }
-      ]
-    };
+    return {};
   },
   mounted() {
 
@@ -75,7 +32,9 @@ export default {
       this.$router.push({path: url})
     }
   },
-  components: {}
+  components: {
+    asideMenu
+  }
 };
 </script>
 
