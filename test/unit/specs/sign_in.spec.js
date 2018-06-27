62069cead697180edf934f953fc6c6030408d46e
HerryLo <13995578046@163.com>
2018-06-27 10:44:10 +0800

    添加依赖

--- test/unit/specs/sign_in.spec.js
+++ test/unit/specs/sign_in.spec.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
+// import Vue from 'vue'
+import { mount } from 'vue-test-utils'
 import SignIn from '@/components/user/sign_in'
 
 describe('sign_in.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(SignIn)
-    const vm = new Constructor().$mount()
-    expect(vm).toEqual('Welcome to Your Vue.js App')
+    const wrapper = mount(SignIn)
+    expect(wrapper.vm.password).toEqual('')
   })
 })
